508b778ab9667389adeeaa0406ee9fcb9e74cb1b
Carl Li <carl_li@yahoo.com>
2024-02-03 10:18:35 +1000

    Initial commit

---
+++ test/vault-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VaultCdk from '../lib/vault-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vault-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VaultCdk.VaultCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
